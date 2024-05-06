25726a8a3c9131ad183d94d6b27a11bfc0f47437
Deefyy <15126011+deefyy@users.noreply.github.com>
2024-05-06 05:12:00 +0000

    Removed code from list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,11 +4,8 @@ import '../App.tsx'
 function List() {
     return (
         <div className={styles.list}>
-            {users.map(user => (
-                <div style={{borderBottom: '1px solid black'}}>
-                    <div>Nazwa: {user.name}</div>
-                </div>
-            ))}
+            
+            <h1>xd</h1>
         </div>
     )
 }
