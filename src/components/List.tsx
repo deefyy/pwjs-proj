f643e650fdfba24ac63cfd5c516ffb33c17744bb
Deefyy <15126011+deefyy@users.noreply.github.com>
2024-05-27 11:36:17 +0000

    Moved code to list.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,10 +1,80 @@
 import styles from './List.module.css'
 import '../App.tsx'
+import { collection, getDocs, addDoc, Timestamp} from "firebase/firestore";
+import { ref, getDownloadURL } from "firebase/storage";
+import { db, storage } from '../firebase-config.ts';
+
+import { useEffect, useState } from "react";
+
+interface User {
+  id: string;
+  name: string;
+  surname: string;
+  date: Timestamp; 
+  avatar: string;
+}
 
 function List() {
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [date, setDate] = useState('')
+    const [imageUrl, setImageUrl] = useState('');
+  
+    async function getUsers() {
+  
+      const querySnapshot = await getDocs(collection(db, "users"));
+  
+      const data: User[] = [];
+      querySnapshot.forEach((doc) => {
+        data.push({
+          id: doc.id,
+          ...(doc.data() as Omit<User, 'id'>)
+        })
+      });
+      setUsers(data)
+      
+    }
+  
+    async function addUsers(e: React.FormEvent<HTMLFormElement>) {
+      e.preventDefault()
+      const docRef = await addDoc(collection(db, "users"), {
+        name,
+        surname,
+        date,
+      });
+  
+      getUsers()
+    }
+  
+    const fetchImageUrl = async () => {
+      const imageRef = ref(storage, 'noavatar.jpg');
+      try {
+          const imageUrl = await getDownloadURL(imageRef);
+          setImageUrl(imageUrl);
+      } catch (error) {
+          console.error('Failed to fetch image:', error);
+          setImageUrl(''); // Set image URL to empty if there's an error
+      }
+    };
+  
+    useEffect(() => {
+      getUsers()
+      fetchImageUrl();
+    }, [])
+
     return (
         <div className={styles.list}>
-        
+        {users.map(user => (
+          <div className={styles.list}>
+            <div className={styles.element}>
+              <img src={imageUrl} alt="No Avatar" style={{ width: '50px', height: '50px' }}/>
+              <div>{user.name}<br></br>{user.surname}</div>
+              <div>{user.date.toDate().toLocaleDateString('en-GB', { year: 'numeric', month: 'numeric', day: 'numeric' })}</div>
+            </div>
+          </div>
+        ))}
         </div>
     )
 }
